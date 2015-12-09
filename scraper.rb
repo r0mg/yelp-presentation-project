233cd7ba10f980fb5417d7208e46f092b4b48780
r0mg <roslynnmg@gmail.com>
2015-12-09 09:37:31 -0500

    most recent version

--- scraper.rb
+++ scraper.rb
@@ -2,17 +2,38 @@ require 'yelp'
 
 client = Yelp::Client.new(
   {
-    consumer_key: "kYUtIL4_JaUTzbVXYYG4eA",
-    consumer_secret: "9SA_nrOUDurOpJ8pjHXwf7y2DhQ",
-    token: "iK8PukN8xRW4WDwemFrDlTiYwLbiPuep",
-    token_secret: "o3x2afabAFd8M8eWCvL8GzLU14Y"
+    consumer_key: "XXXXXXXXXXXXXXXXX",
+    consumer_secret: "XXXXXXXXXXXXXXXXX",
+    token: "XXXXXXXXXXXXXXXXX",
+    token_secret: "XXXXXXXXXXXXXXXXX"
   }
 )
 
-response = client.search('San Francisco')
- 
-response.businesses.each do |business|
-  if business.rating > 3
-    puts "#{business.name}, #{business.rating}"
+  continue = "y"
+    
+  puts "Where ARE you? (Enter a zipcode, neighborhood, city, etc.)"
+  location = gets.chomp
+
+  puts "What type of food would you like? Leave blank if you'd like us to choose ;)"
+  food_type = gets.chomp
+ results = client.search(location,{category_filter: 'restaurants'}, term: food_type, radius_filter: 405)
+  
+   
+  array_of_businesses = results.businesses
+  randomized_array = array_of_businesses.shuffle
+  
+  while continue == "y" && randomized_array.length > 0
+    random_selection = randomized_array.pop
+    puts
+    puts "Congratulations! You're going to " + random_selection.name + "!"
+    puts "Head to " + random_selection.location.display_address.first + " immediately!"
+    puts
+    puts "...or would you rather go somewhere else? Y/N"
+    continue = gets.chomp.downcase
   end
-end
+
+  if continue == "y"
+    puts "You're too picky! There are no more #{food_type} restaurants in your area."
+  else
+    puts "Enjoy!"
+  end
